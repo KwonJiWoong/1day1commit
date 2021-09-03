e2d46d1c6ca9bd8c43e11c575b1336b3715d89ee
권지웅 <32256659+KwonJiWoong@users.noreply.github.com>
2021-09-04 02:21:31 +0900

    2021-09-04 시간이 늦어서 내일 다시 수정(컴포넌트추가)

--- src/component/section.js
+++ src/component/section.js
@@ -2,13 +2,21 @@ import '../css/section.css';
 import React, { useState } from 'react';
 
 function InputSample(){
+  /* welcome문구 state 설정 */
+  const [welcome, addWelcome] = useState({
+    addcompo:[]
+  });
+  /* input state 설정 */
   const [inputs, setInputs] = useState({
     name:'',
     nickname:'',
     text:''
   });
+  /* state 저장 */
   const { name, nickname, text } = inputs;
+  const { addcompo } = welcome;
 
+  /* onChane 이벤트로 state 변경 */
   const onChange = (e) => {
     const { value, name } = e.target;
     setInputs({
@@ -16,7 +24,7 @@ function InputSample(){
       [name]: value
     });
   };
-
+  /* onReset 이벤트로 state 초기화 */
   const onReset = () => {
     setInputs({
       name:'',
@@ -24,7 +32,7 @@ function InputSample(){
       text:''
     });
   };
-
+  /* onSet 이벤트로 state 다시 저장후 return*/
   const onSet = () => {
     setInputs({
       name:name,
@@ -33,6 +41,15 @@ function InputSample(){
     });
       
   };
+  const Welcome = ()=> {
+    return (<h2>"이름은 "{name}"이고 별명은"{nickname}"이시네요!"</h2>);
+  };
+  /* addCom 이벤트로 state 활용한 컴포넌트 추가 */
+  const addCom = () => {
+    addWelcome({
+      ...addWelcome, addcompo: <Welcome />
+    })
+  };
 
   return (
     <div>
@@ -40,10 +57,10 @@ function InputSample(){
       <input name="nickname" placeholder="닉네임"  onChange={onChange} value={nickname}/>
       <button onClick={onReset}>초기화</button>
       <div>
-
         <p>{name} / ({nickname})님 오신걸 환영합니다.</p>
         <button onClick={onSet}>확인</button>
         <p>{text}</p>
+        <button onClick={addCom}>컴포넌트 추가</button>
       </div>
     </div>
   );
