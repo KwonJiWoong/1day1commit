a2d892e94a28ee867d00b38c93b627c661f6bde8
권지웅 <32256659+KwonJiWoong@users.noreply.github.com>
2021-10-13 22:55:24 +0900

    2021-10-13
    업로드

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './css/reset.css';
 import Header from './component/header';
 import Section from './component/section';
 import Footer from './component/footer';
+const { default: fetch } = require("node-fetch");
 
+fetch("https://api.randomuser.me/?nat=US&results=1").then(res =>
+    console.log(res.json())
+);
 function App() {
   
   return (
