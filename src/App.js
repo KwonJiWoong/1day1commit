5da352c217725193f4ba32c1c300d6349463dea3
권지웅 <32256659+KwonJiWoong@users.noreply.github.com>
2021-10-13 23:09:33 +0900

    Revert "2021-10-13"
    This reverts commit a2d892e94a28ee867d00b38c93b627c661f6bde8.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './css/reset.css';
 import Header from './component/header';
 import Section from './component/section';
 import Footer from './component/footer';
-const { default: fetch } = require("node-fetch");
 
-fetch("https://api.randomuser.me/?nat=US&results=1").then(res =>
-    console.log(res.json())
-);
 function App() {
   
   return (
